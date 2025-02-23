43d339b29ed740166457252faa752cdd64c08fdd
ismael2606 <ismaeldavid2010@gmail.com>
2025-02-22 22:51:21 -0800

    project done

--- js/script.js
+++ js/script.js
@@ -15,18 +15,17 @@ quotes = [{quote: "Do what you can, with what you have, where you are.", source:
           {quote: "You may say I'm a dreamer, but I'm not the only one.", source: "John Lennon" }, 
           {quote: "It's not our abilities that show what we truly are... it is our choices.", source: "J.K. Rowling" , citation: " Harry Potter and the Chamber of Secrets" }, 
           {quote:  "I have a dream that one day this nation will rise up and live out the true meaning of its creed.", source: "Martin Luther King Jr." , year: 1963 }
-        ];
+         ];
 
 
 /***
  * `getRandomQuote` function
 ***/
 function getRandomQuote() {
-  let randomNumber = Math.floor(Math.random() * quotes.length + 1);
+  let randomNumber = Math.floor(Math.random() * quotes.length);
   return quotes[randomNumber];
 }
 
-getRandomQuote();
 
 
 /***
@@ -36,9 +35,30 @@ function printQuote() {
   let randomQuote = getRandomQuote();
   let htlmString = `<p class="quote">${randomQuote.quote}</p>
                     <p class="source">${randomQuote.source}`;
-  console.log(htlmString);
+
+  if (randomQuote.citation) {
+    htlmString += ` <span class="citation">${randomQuote.citation}</span></p>`;
+  }
+  if (randomQuote.year) {
+    htlmString += ` <span class="year">${randomQuote.year}</span></p>`;
+  }
+  document.getElementById('quote-box').innerHTML = htlmString; 
 }
-printQuote();
+
+
+
+function randomBackgroundColor() {
+  let colorPicker = () => Math.floor(Math.random() * 256);
+  document.querySelector('body').style.backgroundColor = `rgb(${colorPicker()}, ${colorPicker()}, ${colorPicker()})` 
+} 
+
+// Display random background color when printing new quote
+document.getElementById('load-quote').addEventListener("click", randomBackgroundColor, false);
+
+//Prints a new quote at intervals of 10secs
+setInterval(printQuote, 10000);
+
+
 
 /***
  * click event listener for the print quote button
